bf8aaf63ea92476f2cd25cba944a31144b491b62
huangchunrong <Huangchunrong@leyaoyao.com>
2018-05-17 18:06:34 +0800

    20180517

--- src/App.vue
+++ src/App.vue
@@ -64,15 +64,16 @@ export default {
     })(document, window);
 
     //
-    MtaH5.init({
+    //临时功能注释
+   /* MtaH5.init({
       "sid":process.env.NODE_ENV=='development'?'500613331':'500613187', //必填，统计用的appid
-   /*   "cid":'********', //如果开启自定义事件，此项目为必填，否则不填*/
+   /!*   "cid":'********', //如果开启自定义事件，此项目为必填，否则不填*!/
       "autoReport":0,//是否开启自动上报(1:init完成则上报一次,0:使用pgv方法才上报)
       "senseHash":1, //hash锚点是否进入url统计
       "senseQuery":1, //url参数是否进入url统计
       "performanceMonitor":0,//是否开启性能监控
       "ignoreParams":[] //开启url参数上报时，可忽略部分参数拼接上报
-    });
+    });*/
 
     //
     Vue.tools.wxConfig({
